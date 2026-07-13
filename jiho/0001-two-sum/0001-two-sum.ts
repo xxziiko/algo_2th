8754555ab2446746de0d9775169259efd2808a38
권지호 <xxziiko@gmail.com>
2026-07-13 17:59:31 +0900

    💡 [LTC] Array: 0001-two-sum
    - Time: 4 ms (54.28%)
    - Space: 59.3 MB (7.97%)
    - Difficulty: Easy
    - Date: 07-13-2026

--- jiho/0001-two-sum/0001-two-sum.ts
+++ jiho/0001-two-sum/0001-two-sum.ts
@@ -1,11 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
-    const map = new Map()
+    const map = new Map ()
 
-    for(const [i, num] of nums.entries()){
+    for(const [i, num] of nums.entries()) {
         const rest = target - num
 
         if(map.has(rest)) {
-            return [map.get(rest), i]
+            return [i, map.get(rest)]
         }
 
         map.set(num, i)
